8c63b6a5c79fe09c3bead5a31463b820f879c3b3
DTatarchuk <dtatarchuk@swiftinvention.com>
2021-09-05 18:56:32 +0300

    more flexabilitty error handling

--- src/plugins/Ajv.ts
+++ src/plugins/Ajv.ts
@@ -7,10 +7,15 @@ const ajvPlug = addFormats(ajv);
 
 const convertAjvError = (ajvErr?: ErrorObject[] | null): ValidateError => {
   if (ajvErr && ajvErr.length) {
+    let additionInfo = "";
+    if (!ajvErr[0].instancePath.length) {
+      const match = ajvErr[0].message!.match(/(['])(?:(?=(\\?))\2.)*?\1/gm);
+      additionInfo = match ? match[0].substr(1, match[0].length - 2) : "";
+    }
     return {
       status: "error",
       message: ajvErr[0].message!,
-      field: ajvErr[0].instancePath.substring(1),
+      field: ajvErr[0].instancePath.length ? ajvErr[0].instancePath.substring(1) : additionInfo,
     }
   }
 
